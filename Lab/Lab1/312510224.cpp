2347b44c0c1f8ce8f941e25943658d11ad7c820c
YubiYubi719 <littlebe0719@gmail.com>
2024-07-31 21:45:37 +0800

    test

--- Lab/Lab1/312510224.cpp
+++ Lab/Lab1/312510224.cpp
@@ -1,8 +1,13 @@
 #include "QM.h"
+// #define TESTMODE
+
 #include <ctime>
 
 int main(int argc, char* argv[]){
+    #ifdef TESTMODE
     clock_t start = clock();
+    #endif
+
     QuineMcclusky qm;
     qm.readfile(argv[1]);
     qm.buildImplicationTable();
@@ -19,7 +24,9 @@ int main(int argc, char* argv[]){
     qm.printMinimumCovering(output);
     output.close();
 
+    #ifdef TESTMODE
     cout << (double)(clock()-start) / CLOCKS_PER_SEC * 1000 << "ms" << '\n';
+    #endif
 
     return 0;
 }
