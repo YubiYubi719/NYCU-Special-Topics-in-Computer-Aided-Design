87d3bec470baea2364f9c9c0e54bf5407050d914
YubiYubi719 <littlebe0719@gmail.com>
2024-08-22 13:04:12 +0800

    update verifier

--- Lab/Lab2_Static-Timing-Analysis/verifier_12.cpp
+++ Lab/Lab2_Static-Timing-Analysis/verifier_12.cpp
@@ -2,6 +2,18 @@
 #include <unordered_map>
 using namespace std;
 
+void step1(const string DUT_path, const string GOLDEN_path){
+    ifstream DUT(DUT_path);
+    string curLine;
+    unordered_map<string,int> mp;
+    while(getline(DUT,curLine)){
+        stringstream ss(curLine);
+        string cellName, outputLoad;
+        ss >> cellName >> outputLoad;
+        mp[cellName] = stoi(outputLoad.substr(2));
+    }
+}
+
 int main(int argc, char* argv[]){
     // argv[1]: studentID_caseName_load.txt
     // argv[2]: golden_caseName_load.txt
