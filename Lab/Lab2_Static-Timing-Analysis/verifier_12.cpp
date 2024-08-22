d008ecc275db745a5f8485cb316ae5c525a1b913
YubiYubi719 <littlebe0719@gmail.com>
2024-08-22 11:26:50 +0800

    update verifier

---
+++ Lab/Lab2_Static-Timing-Analysis/verifier_12.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    // argv[1]: studentID_caseName_load.txt
+    // argv[2]: golden_caseName_load.txt
+
+    // argv[3]: studentID_caseName_delay.txt
+    // argv[4]: golden_caseName_delay.txt
+
+    // argv[5]: studentID_caseName_path.txt
+    // argv[6]: golden_caseName_path.txt
+
+    // argv[7]: studentID_caseName_gate_info.txt
+    // argv[8]: golden_caseName_gate_info.txt
+    
+    // ****************
+    // *    Step 1    *
+    // ****************
+
+
+    return 0;
+}
